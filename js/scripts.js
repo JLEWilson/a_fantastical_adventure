5c85f8cbfb2b3060729b822923f94e111eb1dafd
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-25 10:24:18 -0800

    Add loops and arrays
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,22 @@ $(document).ready(function() {
     const nounInput = $("input#noun").val();
 
     const inputArray = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
-    const pushArray = [];
-    pushArray.push(inputArray[1]);
-    pushArray.push(inputArray[0]);
-    pushArray.push(inputArray[2]);
 
-    
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    const tag1 = $(".person1");
+    const tag2 = $(".person2");
+    const tag3 = $(".animal");
+    const tag4 = $(".exclamation");
+    const tag5 = $(".verb");
+    const tag6 = $(".noun");
+
+    const tagArray = [tag1,tag2,tag3,tag4,tag5,tag6];
+    let index = 0;
+    inputArray.forEach(function(tag) {
+      tagArray[index].text(inputArray[index]);
+      index +=1;
+    });
 
     $("#story").show();
-
-    $("ul#outputList").prepend("<li>" + pushArray[0] + "</li>");
-
   });
 });
